d73209946a66a3871a37ef2e2a9b5aae590cae78
An JH <jissps@naver.com>
2017-06-23 19:44:57 +0900

    1

--- js/worker.js
+++ js/worker.js
@@ -4,23 +4,36 @@
  * and open the template in the editor.
  */
 
-self.addEventListener('push', event => {
-	let promiseChain;
-	if (event.data) {
-		// We have data - lets use it
-		promiseChain = Promise.resolve(event.data.json());
-	} else {
-		promiseChain = fetch('/some/data/endpoint.json')
-				.then(response => response.json());
-	}
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  if (event.action === 'confirm') {
+    var fetchOptions = {
+      method: 'post',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: event.notification.data.confirmation_id
+    };
+    var confirmation = new Request('/back/end/system/confirm');
+    event.waitUntil(fetch(confirmation, fetchOptions));
+    return; // So we don't open the page when we don't need to.
+  } else if (event.action === 'change') {
+    var appUrl = '?confirmation_id=' +
+      event.notification.data.confirmation_id + '#reschedule';
+  } else {
+    var appUrl = '/';
+  }
 
-	promiseChain = promiseChain.then(data => {
-		return self.registration.showNotification(data.title, {
-			body: data.body,
-			icon: (data.icon ? data.icon : '/images/icon-192x192.png'),
-			vibrate: [200, 100, 200, 100, 200, 100, 400],
-			tag: data.tag
-		});
-	});
-	event.waitUntil(promiseChain);
+  event.waitUntil(clients.matchAll({
+    includeUncontrolled: true,
+    type: 'window'
+    }).then( activeClients => {
+      if (activeClients.length > 0) {
+        activeClients[0].navigate(appUrl);
+        activeClients[0].focus();
+      } else {
+        clients.openWindow(appUrl);
+      }
+    })
+  );
 });
