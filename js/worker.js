b08a37bb528d4ea70e541f6582d34d4b1cfc9bbf
An JH <jissps@naver.com>
2017-06-23 20:48:21 +0900

    1

--- js/worker.js
+++ js/worker.js
@@ -4,65 +4,20 @@
  * and open the template in the editor.
  */
 
-self.addEventListener('push', function(event) {
-  const promiseChain = getData(event.data)
-  .then(data => {
-    return self.registration.getNotifications({tag: data.tag})
-    .then(notifications => {
-      var noteOptions = {
-        body: data.body,
-        icon: (data.icon ? data.icon : '/images/ic_flight_takeoff_black_24dp_2x.png'),
-        vibrate: [200, 100, 200, 100, 200, 100, 400],
-        tag: data.tag,
-        data: data
-        };
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
 
-      if (notifications.length > 0) {
-        data.title = "Flight Updates";
-        noteOptions.body = "There are several updates regarding your flight, 5212 to Kansas City.";
-        noteOptions.renotify = true;
-        noteOptions.actions = [
-          {action: 'view', title: 'View updates'},
-          {action: 'notNow', title: 'Not now'}
-        ];
-      }
-
-      return self.registration.showNotification(data.title, noteOptions);
-    });
-  });
-  event.waitUntil(promiseChain);
-});
-
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  if (event.action === 'confirm') {
-    var fetchOptions = {
-      method: 'post',
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      }),
-      body: event.notification.data.confirmation_id
-    };
-    var confirmation = new Request('/back/end/system/confirm');
-    event.waitUntil(fetch(confirmation, fetchOptions));
-    return; // So we don't open the page when we don't need to.
-  } else if (event.action === 'change') {
-    var appUrl = '?confirmation_id=' +
-      event.notification.data.confirmation_id + '#reschedule';
-  } else {
-    var appUrl = '/';
-  }
-
-  event.waitUntil(clients.matchAll({
-    includeUncontrolled: true,
-    type: 'window'
-    }).then( activeClients => {
-      if (activeClients.length > 0) {
-        activeClients[0].navigate(appUrl);
-        activeClients[0].focus();
-      } else {
-        clients.openWindow(appUrl);
-      }
-    })
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
